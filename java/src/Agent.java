3176994375fc028db16b36bcdf9a227796ae9715
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-04-24 15:03:31 +0300

    rm unused import

--- java/src/Agent.java
+++ java/src/Agent.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
-import me.zed_0xff.zombie_buddy.patches.experimental.Main;
 import zombie.core.Core;
 
 public class Agent {
